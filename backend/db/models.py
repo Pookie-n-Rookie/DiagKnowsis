9dec51ec2d5cb312e2e884d9a1a7a09b35d6a3e0
unknown <neel26ray@gmail.com>
2026-04-02 01:23:41 +0530

    fix: password data type

--- backend/db/models.py
+++ backend/db/models.py
@@ -6,4 +6,4 @@ class User(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True)
-    password = Column(String)
+    password = Column(String(255))
